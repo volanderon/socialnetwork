403a8add8dd12f7c3aee0adcfb642a0205a0900f
arnold <arnold@gmail.com>
2015-08-26 14:49:33 +0300

    Populate day, month & year selects with values

--- account.php
+++ account.php
@@ -14,16 +14,54 @@
                 Edit your profile information
             </div>
 
-            <div id="">
+            <div id="account-form">
 
-                First Name <input type="text" name="firstName"><br>
-                Last Name <input type="text" name="lastName"><br>
-                Email <input type="email" name="email"><br>
-                Birth Date <input type="date" name="birthDate"><br>
-                Gender <input type="text" name="gender"><br>
-                Location <input type="text" name="location"><br>
-                Website <input type="text" name="firstName"><br>
-                About Me <input type="textarea" name="firstName"><br>
+                <div>
+                    <div>First Name</div>
+                    <input type="text" name="firstName">
+                    <div>Enter your first name</div>
+                </div>
+                <div>
+                    <div>Last Name</div>
+                    <input type="text" name="lastName">
+                    <div>Enter your last name</div>
+                </div>
+                <div>
+                    <div>Email</div>
+                    <input type="email" name="email">
+                    <div>E-mail will not be displayed</div>
+                </div>
+                <div>
+                    <div>Birth Date</div>
+                    <select id="birth-day" name="birthDay"></select>
+                    <select id="birth-month" name="birthMonth"></select>
+                    <select id="birth-year" name="birthYear"></select>
+                    <div>Select the date you were born</div>
+                </div>
+                <div>
+                    <div>Gender</div>
+                    <select name="gender">
+                        <option value="0">No Gender</option>
+                        <option value="1">Male</option>
+                        <option value="2">Female</option>
+                    </select>
+                    <div>Select your gender (male or female)</div>
+                </div>
+                <div>
+                    <div>Location</div>
+                    <input type="text" name="location">
+                    <div>Where do you live?</div>
+                </div>
+                <div>
+                    <div>Website</div>
+                    <input type="url" name="firstName">
+                    <div>If you have a blog, personal page, enter it</div>
+                </div>
+                <div>
+                    <div>About Me</div>
+                    <textarea rows="5" cols="50" name="firstName"></textarea>
+                    <div>About you (160 characters or less)</div>
+                </div>
                 <button name="saveChanges">Save Changes</button>
 
 
--- script/account.js
+++ script/account.js
@@ -1,7 +1,26 @@
 var Account = {
-
+    /**
+     * Populate day, month & year selects with values
+     */
+    populateBirthdaySelects: function() {
+        var days_html = '', months_html = '', years_html = '',
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            current_year = new Date().getFullYear();
+        for (var i = 1; i <= 31; i++) {
+            days_html += '<option value="' + i + '">' + i + '</option>';
+        }
+        for (var j = 0; j <= 11; j++) {
+            months_html += '<option value="' + months[j] + '">' + months[j] + '</option>';
+        }
+        for (var k = current_year; k >= current_year - 100; k--) {
+            years_html += '<option value="' + k + '">' + k + '</option>';
+        }
+        $('#birth-day').html(days_html);
+        $('#birth-month').html(months_html);
+        $('#birth-year').html(years_html);
+    }
 };
 
 $(function() {
-
+    Account.populateBirthdaySelects();
 });
