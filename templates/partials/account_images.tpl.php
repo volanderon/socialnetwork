9b0e4a219a54a7a24a932cc06be72ea849a88d58
arnold <arnold@gmail.com>
2015-08-30 16:49:58 +0300

    upload image design

--- templates/partials/account_images.tpl.php
+++ templates/partials/account_images.tpl.php
@@ -3,23 +3,37 @@
         <img class="box-user-icon" src="user_content/photos/<?php echo $_SESSION['auth']['user_profile_picture']; ?>">
         Profile Settings
     </div>
+
     <div class="box-subTitle">
         <div class="success">
             Success
         </div>
         Change your account picture
     </div>
-    <div class="box-subTitle">
+
+    <div class="box-updateImg">
+
         <img id="photo-img" src="user_content/photos/<?php echo $_SESSION['auth']['user_profile_picture']; ?>">
-        <input id="photo-input" type="file" name="photo">
-        <button id="upload-photo-btn" name="uploadPhoto">Upload Photo</button>
+
+        <div class="updateImg">
+            <input id="photo-input" type="file" name="photo"><br>
+            <button id="upload-photo-btn" name="uploadPhoto">Upload Photo</button>
+        </div>
+
     </div>
+
     <div class="box-subTitle">
         Change your cover picture
     </div>
-    <div class="box-subTitle">
+
+    <div class="box-updateImg">
+
         <img id="cover-img" src="user_content/covers/<?php echo $_SESSION['auth']['user_secret_picture']; ?>">
-        <input id="cover-input" type="file" name="cover">
-        <button id="upload-cover-btn" name="uploadCover">Upload Cover</button>
+
+        <div class="updateImg">
+            <input id="cover-input" type="file" name="cover"><br>
+            <button id="upload-cover-btn" name="uploadCover">Upload Cover</button>
+        </div>
+
     </div>
 </div>
